1642c6f8ef09c31612df3eb4162a40d722594a93
Mudasirh120 <mudasirh120@gmail.com>
2025-08-17 23:29:11 +0500

    cron enabled and public path corrected

--- server.js
+++ server.js
@@ -8,13 +8,14 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 import fileRouter from "./route/fileRoute.js";
 import connectCloudinary from "./config/cloudinaryConfig.js";
-// import cron from "./util/cron.js";
+import cron from "./util/cron.js";
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use("/file", fileRouter);
 app.get("/", (req, res) => {
   res.render("index");
